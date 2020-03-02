4016d4e7ea4594d185b783dfe6a590d515864ffc
Sarah Thompson <sarahloisthompson@gmail.com>
2020-03-01 20:51:03 -0600

    Updates

---
+++ precache-manifest.0f03a1b557c99d2b7625345fbcb34f79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c473e8060fb6bb35d3a3bd5caa6bfee",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "a36b391ee00d051cf7e5",
+    "url": "/react-site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bef718aa79d2420ba859",
+    "url": "/react-site/static/js/2.874e5ab1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-site/static/js/2.874e5ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36b391ee00d051cf7e5",
+    "url": "/react-site/static/js/main.c15f239a.chunk.js"
+  },
+  {
+    "revision": "231cd30340de89c25fd7",
+    "url": "/react-site/static/js/runtime-main.8c209044.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
