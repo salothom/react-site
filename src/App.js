9606045ee118c1502aca4f29c1b16c2541ca6e00
Sarah Thompson <sarahloisthompson@gmail.com>
2020-04-25 19:17:44 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -24,14 +24,11 @@ function App() {
   );
 }
 
-
-
 function importAll(r) {
   let images = {};
-  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
   return images;
 }
-
 
 class MainHome extends React.Component {
   constructor(props) {
@@ -50,19 +47,9 @@ class MainHome extends React.Component {
     }
   }
 
-
   render() {
-    const homeShow = this.state.homeShow;
-    let shown;
     let main;
-    // if (homeShow) {
-    //   shown = <button className="mainButton" onClick={this.handleSwitchClick} >View Library</button>;
-    //   main = <Graph />
-    // } else {
-    shown = <button className="mainButton" onClick={this.handleSwitchClick} >View Main</button>;
     main = <BookShelf />;
-    // }
-
     return (
       <div className="homeMain">
         {/* <div>{shown}</div> */}
@@ -72,11 +59,7 @@ class MainHome extends React.Component {
   }
 }
 
-class Modal extends React.Component {
-  render() {
-    return (<div >Hello Modal</div>);
-  }
-}
+
 
 class BookShelf extends React.Component {
   constructor(props) {
@@ -161,31 +144,31 @@ class BookShelf extends React.Component {
           <div className="modalShadow" onClick={this.handleModal.bind(this, this.state.book)}> </div>
           <div className="bookModal">
             {/* <div className="heightAll"> */}
-              <div className="modalHeader">
-                <h2 > {this.state.book.title}</h2>
-                <h5>{this.state.book.author}</h5>
+            <div className="modalHeader">
+              <h2 > {this.state.book.title}</h2>
+              <h5>{this.state.book.author}</h5>
+            </div>
+            <div className="modalBody">
+              <div style={{ marginTop: 5 + 'px' }} className="col-6-m">
+                <img alt="books" style={{ width: 70 + '%' }} src={images[this.state.book.image]} />
               </div>
-              <div className="modalBody">
-                <div style={{ marginTop: 5 + 'px' }} className="col-6-m">
-                  <img style={{ width: 70 + '%' }} src={images[this.state.book.image]} />
-                </div>
 
-                <div className="col-6-m">
-                  <p ngIf="this.state.book.subtitle">{this.state.book.subtitle}</p>
-                  <div>Length: {this.state.book.pages}</div>
-                  <div> Published in {this.state.book.published} </div>
+              <div className="col-6-m">
+                <p ngIf="this.state.book.subtitle">{this.state.book.subtitle}</p>
+                <div>Length: {this.state.book.pages}</div>
+                <div> Published in {this.state.book.published} </div>
 
-                  <div>
-                    {this.renderGenres()}
-                  </div>
-
+                <div>
+                  {this.renderGenres()}
                 </div>
 
               </div>
+
+            </div>
 
             <div className="closeModal" onClick={this.handleModal.bind(this, this.state.book)} >Close</div>
 
-            </div>
+          </div>
           {/* </div> */}
         </div>
 
@@ -244,7 +227,7 @@ class BookShelf extends React.Component {
       booksDisplay.push(
         <div className="bookImages" >
           <div onClick={this.handleModal.bind(this, this.state.booklist[i])} style={{ margin: 5 + "px" }}>
-            <img src={images[this.state.booklist[i].image]} />
+            <img alt="books" src={images[this.state.booklist[i].image]} />
           </div>
         </div>
       );
@@ -281,84 +264,84 @@ class BookShelf extends React.Component {
 
 
 
-class Graph extends React.Component {
-  state = {
-    coolThings: [
-      { name: 'Blueberry Tea', coolness: '87' },
-      { name: 'Chocolate Soft Serve w/o Sprinkles', coolness: '7' },
-      { name: 'Vanilla Soft Serve w/ Sprinkles', coolness: '83' },
-      { name: 'People using ellipses in emails', coolness: '17' },
-      { name: 'Public Library Cards', coolness: '92' },
-      { name: 'The Last Slice of Pie', coolness: '62' },
-      { name: 'Having a whole Pie to share', coolness: '95' }
-    ]
-  }
+// class Graph extends React.Component {
+//   state = {
+//     coolThings: [
+//       { name: 'Blueberry Tea', coolness: '87' },
+//       { name: 'Chocolate Soft Serve w/o Sprinkles', coolness: '7' },
+//       { name: 'Vanilla Soft Serve w/ Sprinkles', coolness: '83' },
+//       { name: 'People using ellipses in emails', coolness: '17' },
+//       { name: 'Public Library Cards', coolness: '92' },
+//       { name: 'The Last Slice of Pie', coolness: '62' },
+//       { name: 'Having a whole Pie to share', coolness: '95' }
+//     ]
+//   }
 
 
-  renderLines() {
-    return Array(12).fill(null).map((el, i) => (
-      <Line left={i * (100 / 12)} key={i} />
-    ));
-  }
-  renderBars() {
-    // const { coolThings } = this.state.coolThings;
-    const coolLength = this.state.coolThings.length;
-    let bars = [];
-    for (let i = 0; i < coolLength; i++) {
-      bars.push(<Bar percent={this.state.coolThings[i].coolness} label={this.state.coolThings[i].name} />
-      )
-    }
-    return bars;
-  }
-  render() {
-    return (
-      <div className="graph-wrapper">
-        <h4>How Great Some Things Are*</h4>
+//   renderLines() {
+//     return Array(12).fill(null).map((el, i) => (
+//       <Line left={i * (100 / 12)} key={i} />
+//     ));
+//   }
+//   renderBars() {
+//     // const { coolThings } = this.state.coolThings;
+//     const coolLength = this.state.coolThings.length;
+//     let bars = [];
+//     for (let i = 0; i < coolLength; i++) {
+//       bars.push(<Bar percent={this.state.coolThings[i].coolness} label={this.state.coolThings[i].name} />
+//       )
+//     }
+//     return bars;
+//   }
+//   render() {
+//     return (
+//       <div className="graph-wrapper">
+//         <h4>How Great Some Things Are*</h4>
 
-        <div className="graph">
-          {/* <BarTextContext labels={this.state.coolThings} /> */}
-          <div className="bar-lines-container">
-            {this.renderLines()}
-            {this.renderBars()}
+//         <div className="graph">
+//           {/* <BarTextContext labels={this.state.coolThings} /> */}
+//           <div className="bar-lines-container">
+//             {this.renderLines()}
+//             {this.renderBars()}
 
-          </div >
-        </div>
-        <p>*Source: Me</p>
-      </div>
-    )
-  }
-}
-
-
-// const BarTextContext = ({ labels }) => {
-//   return (
-//     <div className="bar-text-context">
-//       {/* {
-//       labels.map((labels)=>(
-//         <div className="text">
-//       {labels.name}
+//           </div >
 //         </div>
-//       ))
-//     } */}
-//     </div>
-//   )
+//         <p>*Source: Me</p>
+//       </div>
+//     )
+//   }
 // }
 
-const Bar = ({ percent, label }) => {
-  // console.log(label);
-  return (
-    <div className="labelCombo">
-      <span className="label">{label}</span>
-      <div className="bar" style={{ width: (percent * .833) + '%' }} />
-    </div>
 
-  )
-}
-const Line = ({ left }) => {
-  return (
-    <div className="line" style={{ left: left + '%' }} />
-  )
-}
+// // const BarTextContext = ({ labels }) => {
+// //   return (
+// //     <div className="bar-text-context">
+// //       {/* {
+// //       labels.map((labels)=>(
+// //         <div className="text">
+// //       {labels.name}
+// //         </div>
+// //       ))
+// //     } */}
+// //     </div>
+// //   )
+// // }
+
+// const Bar = ({ percent, label }) => {
+//   // console.log(label);
+//   return (
+//     <div className="labelCombo">
+//       <span className="label">{label}</span>
+//       <div className="bar" style={{ width: (percent * .833) + '%' }} />
+//     </div>
+
+//   )
+// }
+// const Line = ({ left }) => {
+//   return (
+//     <div className="line" style={{ left: left + '%' }} />
+//   )
+// }
 
 
 
